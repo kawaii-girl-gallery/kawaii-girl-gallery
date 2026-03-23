7827ce477f529f244b219bb437d9f96d2097014d
kawaii-girl-gallery <salada1203@yahoo.co.jp>
2026-03-24 05:12:20 +0900

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -40,13 +40,14 @@ def process_product_image(img_path):
 
         # 日本語フォント（スタンプに使用）
         def load_font_ja(size):
+            # リポジトリに含めたフォントを優先
+            import django.conf
+            base_dir = django.conf.settings.BASE_DIR
             font_paths = [
+                os.path.join(base_dir, "catalog", "static", "fonts", "NotoSansJP-Bold.ttf"),
+                os.path.join(base_dir, "catalog", "static", "fonts", "NotoSansCJK-Bold.ttc"),
                 "/usr/share/fonts/truetype/noto/NotoSansCJK-Bold.ttc",
                 "/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc",
-                "/usr/share/fonts/noto-cjk/NotoSansCJK-Bold.ttc",
-                "/usr/share/fonts/truetype/noto/NotoSansJP-Bold.ttf",
-                "/usr/share/fonts/truetype/vlgothic/VL-Gothic-Regular.ttf",
-                "/usr/share/fonts/truetype/ipafont/ipagp.ttf",
                 "meiryo.ttc",
                 "msgothic.ttc",
             ]
